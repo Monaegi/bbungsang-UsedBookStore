afb448a480d4949a2dc8ac9bf78d7b7bb1b3b74a
bbungsang <821heejin@naver.com>
2017-09-24 00:51:34 +0900

    side menu css 변경, 파는책 등록시 구매 목록에 매칭되는 책 존재하면 쪽지 알람 발송 기능 구현, Seller 등록하기 템플릿 태그 연동

--- django_app/book/views/matching_sell.py
+++ django_app/book/views/matching_sell.py
@@ -1,9 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render
+from django_messages.forms import ComposeForm
 
 from book.forms.book_register import SellBookRegisterForm
 from book.forms.searchs import NaverBooksSearchForm
-from book.models import SellBookRegister
+from book.models import SellBookRegister, Book, BuyBookRegister
+
+MyUser = get_user_model()
 
 
 def sell_book_register(request, ):
@@ -14,7 +18,23 @@ def sell_book_register(request, ):
         if form.is_valid():
             form.save(seller=request.user.my_seller)
 
-            return HttpResponse('성공!')
+            sell_isbn = form.data.get('isbn')
+            book_info = Book.objects.get(isbn=sell_isbn)
+
+            if BuyBookRegister.objects.filter(book_info_id=book_info.pk):
+                data = {
+                    'recipient': request.user.username,
+                    'subject': '안녕하세요!',
+                    'body': '{}님이 판매하시려는 책의 구매 리스트에 존재합니다. 구매 리스트를 확인해보세요 :-)'.format(request.user.username),
+                }
+                compose_form = ComposeForm(data)
+
+                sender = MyUser.objects.get(pk=1)
+
+                if compose_form.is_valid():
+                    compose_form.save(sender=sender)
+
+                    return HttpResponse('성공!')
 
     register_form = SellBookRegisterForm()
     search_form = NaverBooksSearchForm()
