e3ca5bf69ff138e2b2a867a3ba4462983549bb17
bbungsang <821heejin@naver.com>
2017-10-07 00:06:04 +0900

    aws rds 설정후 settings deploy 작성

--- django_app/config/settings/deploy.py
+++ django_app/config/settings/deploy.py
@@ -1,3 +1,10 @@
 from .base import *
 
 config_secret_deploy = json.loads(open(CONFIG_SECRET_DEPLOY_FILE).read())
+
+# debug 모드, allowed hosts
+DEBUG = False
+ALLOWED_HOSTS = config_secret_deploy['django']['allowed_hosts']
+
+# Database
+DATABASES = config_secret_deploy['django']['database']
