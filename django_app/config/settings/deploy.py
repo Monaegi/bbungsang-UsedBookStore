16a885b30c20b2a8775375617f1f8ae3a8c39aee
bbungsang <821heejin@naver.com>
2017-10-10 16:24:10 +0900

    change site url

--- django_app/config/settings/deploy.py
+++ django_app/config/settings/deploy.py
@@ -25,5 +25,5 @@ ALLOWED_HOSTS = config_secret_deploy['django']['allowed_hosts']
 # Database
 DATABASES = config_secret_deploy['django']['databases']
 
-SITE_URL = 'usedbookstore-dev.ap-northeast-2.elasticbeanstalk.com'
+SITE_URL = 'http://usedbookstore-dev.ap-northeast-2.elasticbeanstalk.com'
 KAKAO_REDIRECT_URI = SITE_URL + '/member/login/kakao'
