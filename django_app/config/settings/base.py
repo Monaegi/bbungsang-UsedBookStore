2ac099a96771d38f0bec53fe44828a77137b3ced
bbungsang <821heejin@naver.com>
2017-10-12 00:16:18 +0900

    deploy.txt 수정

--- django_app/config/settings/base.py
+++ django_app/config/settings/base.py
@@ -23,11 +23,6 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
-
-# django_app/media
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 ROOT_DIR = os.path.dirname(BASE_DIR)
 
@@ -171,15 +166,9 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
-STATIC_URL = '/static/'
-
 # Email_Backend
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = config_secret_common['django']['email_host_user']
 EMAIL_HOST_PASSWORD = config_secret_common['django']['email_host_password']
 EMAIL_PORT = '587'
-
