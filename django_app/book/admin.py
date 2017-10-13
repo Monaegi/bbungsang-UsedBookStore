56c5ed29890d4fd2ea16a6f032bd303d3ac64a25
bbungsang <821heejin@naver.com>
2017-10-13 15:45:07 +0900

    책 정보, 판매 도서 관련 어드민 작성

--- django_app/book/admin.py
+++ django_app/book/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
+from book.models import Book, SellBookRegister
+
 MyUser = get_user_model()
 
 
 @admin.register(MyUser)
 class MyUserAdmin(admin.ModelAdmin):
     list_display = ['username', 'nickname', ]
+
+
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+class SellBookRegisterAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(SellBookRegister, SellBookRegisterAdmin)
