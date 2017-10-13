e848864b2f617ed445f2732a34c3ff04c94971cf
bbungsang <821heejin@naver.com>
2017-10-13 16:40:11 +0900

    메인페이지 테스트용 코드 추가

--- django_app/book/views/main.py
+++ django_app/book/views/main.py
@@ -6,8 +6,9 @@ from book.models import SellBookRegister
 def main(request, ):
 
     new_books = SellBookRegister.objects.all()
-
+    bonobono = SellBookRegister.objects.get(used_price='7000')
     context = {
         'new_books': new_books,
+        'bonobono': bonobono,
     }
     return render(request, 'common/main.html', context)
