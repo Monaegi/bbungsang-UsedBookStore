85883bec4fc05fcabdf7959af8a45e92e23ac91b
bbungsang <821heejin@naver.com>
2017-11-03 20:02:48 +0900

    ELB Health check 4xx 에러 해결 코드 추가

--- django_app/config/settings/deploy.py
+++ django_app/config/settings/deploy.py
@@ -28,6 +28,38 @@ STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
 DEBUG = True
 ALLOWED_HOSTS = config_secret_deploy['django']['allowed_hosts']
 
+
+def is_ec2_linux():
+    """Detect if we are running on an EC2 Linux Instance
+       See http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/identify_ec2_instances.html
+    """
+    if os.path.isfile("/sys/hypervisor/uuid"):
+        with open("/sys/hypervisor/uuid") as f:
+            uuid = f.read()
+            return uuid.startswith("ec2")
+    return False
+
+
+def get_linux_ec2_private_ip():
+    """Get the private IP Address of the machine if running on an EC2 linux server"""
+    from urllib.request import urlopen
+    if not is_ec2_linux():
+        return None
+    try:
+        response = urlopen('http://169.254.169.254/latest/meta-data/local-ipv4')
+        ec2_ip = response.read().decode('utf-8')
+        if response:
+            response.close()
+        return ec2_ip
+    except Exception as e:
+        print(e)
+        return None
+
+
+private_ip = get_linux_ec2_private_ip()
+if private_ip:
+    ALLOWED_HOSTS.append(private_ip)
+
 # Database
 DATABASES = config_secret_deploy['django']['databases']
 
